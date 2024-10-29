721b0e7a78bd9cd87d692b2f20e5c3a7bb3990cf
YashAgarwal1201 <legoyashx@gmail.com>
2024-10-29 19:51:41 +0530

    fix: navigation fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
@@ -8,7 +10,15 @@ import { MsgAppContextProvider } from "./Services/MessagesContextAndInterfaces/M
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AppContextProvider>
     <MsgAppContextProvider>
-      <RouterProvider router={Router} />
+      <Suspense
+        fallback={
+          <div className="w-screen h-screen bg-color1 text-color1">
+            Loading...
+          </div>
+        }
+      >
+        <RouterProvider router={Router} />
+      </Suspense>
     </MsgAppContextProvider>
   </AppContextProvider>
 );
